dc6fcb98f3df545e522a458f3790aff4c797e1c1
William Kimball <wkkimball043@gmail.com>
2018-06-04 17:39:02 -0500

    initial commit, practice completed

---
+++ database.js
@@ -0,0 +1,69 @@
+
+const loadDatabase = function (HomeInventoryDatabase) {
+    // Get the string version of the database
+    const databaseString = localStorage.getItem(HomeInventoryDatabase)
+
+    // Use JSON.parse() to convert the string back into an object
+    return JSON.parse(databaseString)
+}
+
+let run = loadDatabase("HomeInventoryDatabase");
+
+// console.log(run);
+
+
+
+function makeData = (dataType) => {
+    for (let obj in run) {
+
+        let array = run[obj];
+        for (i = 0; i < array.length; i++) {
+            let sec = document.createElement("section");
+            let para1 = document.createElement('p');
+            let para2 = document.createElement('p');
+            let para3 = document.createElement('p');
+            para1.textContent = `Name: ${array[i].name}`;
+            para2.textContent = `Location ${array[i].location}`;
+            para3.textContent = `Description: ${array[i].description}`;
+            sec.appendChild(para1);
+            sec.appendChild(para2);
+            sec.appendChild(para3);
+            document.querySelector("#myStuff").appendChild(sec)
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (key in run.furniture[0]) {
+//     let p = document.createElement("p");
+//     p.textContent = run.furniture[0][key]
+//     console.log(p)
+// }
+
+
+// Object.keys(run).forEach(function() {
+//     let sec = document.createElement("section");
+//     let para = document.createElement("p")
+
+//     sec.appendChild(para);
+
+//     document.querySelector("#myStuff").appendChild(sec);
+// })
---
+++ storage.js
@@ -0,0 +1,83 @@
+// Define the database as an object
+const HomeInventoryDatabase = {};
+// Define the arrays (a.k.a. tables) in the database to store each type of items
+HomeInventoryDatabase.furniture = [];
+HomeInventoryDatabase.crafts = [];
+HomeInventoryDatabase.electronics = [];
+
+// The ink well is a craft data item
+const vintageInkwell = {
+    name: "Vintage Ink Well",
+    location: "Writing desk",
+    description: "I enjoy this inkwell because it belonged to my grandfather and holds enough ink to survive weeks of writing."
+  }
+
+  const typingDesk = {
+    name: "Typewriter Desk",
+    location: "Bedroom",
+    description: "This antique desk is special because I found and purchased in North Louisiana early one morning in the winter."
+  }
+
+  const fountainPen = {
+      name: "Parker '51'",
+      location: "Pen case",
+      description: "This is a 1946 Parker '51' Aero with a factory medium nib, and in the Ultra-rare color plum."
+  }
+
+  const bookshelf = {
+    name: "Ikea Bookshelf",
+    location: "Bedroom",
+    description: "This is an Ikea bookshelf. It has some crazy Sweedish name."
+}
+
+const vintageFan = {
+    name: "Vintage Fan",
+    location: "Closet",
+    description: "This is a vintage brass-bladed fan from the 30's. It's super dangerous."
+}
+const computer = {
+    name: "Macbook Pro",
+    location: "Desk",
+    description: "This is a 2017 Macbook pro 13 inch."
+}
+const gameCube = {
+    name: "Nintendo GameCube",
+    location: "Bookshelf",
+    description: "This is a Nintendo GameCube. It plays games."
+}
+const bible = {
+    name: "Cambridge Clarion ESV",
+    location: "Bedside table",
+    description: "This is a Cambridge Clarion ESV bible bound in black goatskin."
+}
+const ghibliMovies = {
+    name: "Studio Ghibli Collection",
+    location: "Bookshelf",
+    description: "This is a collection of most Studio Ghibli movies on Blu-ray."
+}
+const trainAd = {
+    name: "Vintage train ad",
+    location: "Wall",
+    description: "This is a vintage ad for a steam locomotive."
+}
+
+HomeInventoryDatabase.crafts.push(vintageInkwell, fountainPen, trainAd, bible)
+HomeInventoryDatabase.furniture.push(typingDesk, bookshelf)
+HomeInventoryDatabase.electronics.push(ghibliMovies, gameCube, computer, vintageFan)
+
+// console.log(HomeInventoryDatabase.electronics, HomeInventoryDatabase.crafts, HomeInventoryDatabase.furniture)
+
+const saveDatabase = function (databaseObject, localStorageKey) {
+    /*
+        Convert the Object into a string.
+    */
+    const stringifiedDatabase = JSON.stringify(databaseObject)
+
+    /*
+        Create a key in local storage, and store the string
+        version of your inventory database as the value
+    */
+    localStorage.setItem(localStorageKey, stringifiedDatabase)
+}
+
+saveDatabase(HomeInventoryDatabase, "HomeInventoryDatabase")
