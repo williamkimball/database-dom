bcbaec7f16bdf9867035ade51585548858f9b090
William Kimball <wkkimball043@gmail.com>
2018-06-04 20:00:47 -0500

    still working on getting the functions to access data in correct order

--- database.js
+++ database.js
@@ -21,23 +21,47 @@ let makeData = (dataSet) => {
         for (i = 0; i < array.length; i++) {
             let sec = document.createElement("section");
             if (dataSet === "crafts") {
-                console.log(array)
+
                 let para1 = document.createElement('p');
-                para1.textContent = `Name: ${array[i].name}`
-                sec.appendChild(para1);
-                document.querySelector("#myStuff").appendChild(sec)
-            } else if (dataSet === "furniture") {
                 let para2 = document.createElement('p');
-                para2.textContent = `Name: ${array[i].name}`
-                sec.appendChild(para2);
-                document.querySelector("#myStuff").appendChild(sec)
-            }
-            else if (dataSet === "electronics") {
                 let para3 = document.createElement('p');
-                para3.textContent = `Name: ${array[i].name}`
+                para1.textContent = `Name: ${array[0].name}`
+                para2.textContent = `Location ${array[0].location}`;
+                para3.textContent = `Description: ${array[0].description}`;
+                sec.appendChild(para1);
+                sec.appendChild(para2);
                 sec.appendChild(para3);
                 document.querySelector("#myStuff").appendChild(sec)
+                break;
+            } else if (dataSet === "furniture") {
+
+                let para1 = document.createElement('p');
+                let para2 = document.createElement('p');
+                let para3 = document.createElement('p');
+                para1.textContent = `Name: ${array[1].name}`
+                para2.textContent = `Location ${array[1].location}`;
+                para3.textContent = `Description: ${array[1].description}`;
+                sec.appendChild(para1);
+                sec.appendChild(para2);
+                sec.appendChild(para3);
+                document.querySelector("#myStuff").appendChild(sec)
+                break;
+            }
+            else if (dataSet === "electronics") {
+
+                let para1 = document.createElement('p');
+                let para2 = document.createElement('p');
+                let para3 = document.createElement('p');
+                para1.textContent = `Name: ${array[i].name}`
+                para2.textContent = `Location ${array[i].location}`;
+                para3.textContent = `Description: ${array[i].description}`;
+                sec.appendChild(para1);
+                sec.appendChild(para2);
+                sec.appendChild(para3);
+                document.querySelector("#myStuff").appendChild(sec)
+                break;
             } else {
+
                 let para1 = document.createElement('p');
                 let para2 = document.createElement('p');
                 let para3 = document.createElement('p');
@@ -51,6 +75,7 @@ let makeData = (dataSet) => {
                 sec.appendChild(para3);
 
                 document.querySelector("#myStuff").appendChild(sec)
+
             }
         }
 
