4bdf22f1e192152803a50f194b551b69d6f87f82
William Kimball <wkkimball043@gmail.com>
2018-06-05 11:21:31 -0500

    added js file for adding item to database

---
+++ addItem.js
@@ -0,0 +1,7 @@
+document.querySelector("#addItem").onclick = function(){
+let name = document.querySelector("#itemName");
+let location = document.querySelector("#itemLocation");
+let description = document.querySelector("#itemDescription");
+
+
+};
