d227a6acd0e5b0c0d061822b6150807446a65b2c
William Kimball <wkkimball043@gmail.com>
2018-06-05 10:21:20 -0500

    finished making all sections work. All that's left is to refactor

--- database.js
+++ database.js
@@ -12,87 +12,91 @@ let run = loadDatabase("HomeInventoryDatabase");
 
 
 let makeData = (dataSet) => {
-
+    const fragment = document.createDocumentFragment();
+    const mkPara = () => {
+        return document.createElement('p');
+    }
+    const mkSec = () => {
+        return document.createElement("section")
+    }
 
     if (dataSet === "crafts") {
         let array = run.crafts;
-        let sec = document.createElement("section");
-        console.log(array)
+
         array.forEach(element => {
-            console.log(element)
-            let para1 = document.createElement('p');
-            let para2 = document.createElement('p');
-            let para3 = document.createElement('p');
+            let para1 = mkPara();
+            let para2 = mkPara();
+            let para3 = mkPara();
+            let sec = mkSec();
+
             para1.textContent = `Name: ${element.name}`
             para2.textContent = `Location ${element.location}`;
             para3.textContent = `Description: ${element.description}`;
             sec.appendChild(para1);
             sec.appendChild(para2);
             sec.appendChild(para3);
-            document.querySelector("#myStuff").appendChild(sec)
+            fragment.appendChild(sec)
         }
         );
     }
     else if (dataSet === "furniture") {
         let array = run.furniture;
-        let sec = document.createElement("section");
-        console.log(array)
+        
         array.forEach(element => {
-            console.log(element)
-            let para1 = document.createElement('p');
-            let para2 = document.createElement('p');
-            let para3 = document.createElement('p');
+            let sec = mkSec();
+            let para1 = mkPara();
+            let para2 = mkPara();
+            let para3 = mkPara();
             para1.textContent = `Name: ${element.name}`
             para2.textContent = `Location ${element.location}`;
             para3.textContent = `Description: ${element.description}`;
             sec.appendChild(para1);
             sec.appendChild(para2);
             sec.appendChild(para3);
-            document.querySelector("#myStuff").appendChild(sec)
+            fragment.appendChild(sec)
         }
         );
     }
     else if (dataSet === "electronics") {
         let array = run.electronics;
-        let sec = document.createElement("section");
-        console.log(array)
+        
         array.forEach(element => {
-            console.log(element)
-            let para1 = document.createElement('p');
-            let para2 = document.createElement('p');
-            let para3 = document.createElement('p');
+            let sec = mkSec();
+            let para1 = mkPara();
+            let para2 = mkPara();
+            let para3 = mkPara();
             para1.textContent = `Name: ${element.name}`
             para2.textContent = `Location ${element.location}`;
             para3.textContent = `Description: ${element.description}`;
             sec.appendChild(para1);
             sec.appendChild(para2);
             sec.appendChild(para3);
-            document.querySelector("#myStuff").appendChild(sec)
+            fragment.appendChild(sec)
         }
         );
     } else {
         for (let obj in run) {
-        let array = run[obj];
-        let sec = document.createElement("section");
-        for (i = 0; i < array.length; i++) {
+            let array = run[obj];
             
-            let para1 = document.createElement('p');
-            let para2 = document.createElement('p');
-            let para3 = document.createElement('p');
+            for (i = 0; i < array.length; i++) {
+                let sec = mkSec();
+                let para1 = mkPara();
+                let para2 = mkPara();
+                let para3 = mkPara();
 
-            para1.textContent = `Name: ${array[i].name}`
-            para2.textContent = `Location ${array[i].location}`;
-            para3.textContent = `Description: ${array[i].description}`;
+                para1.textContent = `Name: ${array[i].name}`
+                para2.textContent = `Location ${array[i].location}`;
+                para3.textContent = `Description: ${array[i].description}`;
 
-            sec.appendChild(para1);
-            sec.appendChild(para2);
-            sec.appendChild(para3);
+                sec.appendChild(para1);
+                sec.appendChild(para2);
+                sec.appendChild(para3);
 
-            document.querySelector("#myStuff").appendChild(sec)
+                fragment.appendChild(sec)
             }
 
-    }
-}
+        }
+    } document.querySelector('#myStuff').appendChild(fragment)
 }
 
 
