fdb28a8ac85bcd5c48e98f983cfb87ee0b4fcd2d
William Kimball <wkkimball043@gmail.com>
2018-06-04 20:25:13 -0500

    got advanced practice to partially work

--- database.js
+++ database.js
@@ -9,7 +9,7 @@ const loadDatabase = function (HomeInventoryDatabase) {
 
 let run = loadDatabase("HomeInventoryDatabase");
 
-console.log(run);
+console.log(run.crafts[0]);
 
 
 
@@ -17,41 +17,25 @@ let makeData = (dataSet) => {
     for (let obj in run) {
 
         let array = run[obj];
+        let para1 = document.createElement('p');
+        let para2 = document.createElement('p');
+        let para3 = document.createElement('p');
+
 
         for (i = 0; i < array.length; i++) {
             let sec = document.createElement("section");
             if (dataSet === "crafts") {
 
-                let para1 = document.createElement('p');
-                let para2 = document.createElement('p');
-                let para3 = document.createElement('p');
-                para1.textContent = `Name: ${array[0].name}`
-                para2.textContent = `Location ${array[0].location}`;
-                para3.textContent = `Description: ${array[0].description}`;
+                para1.textContent = `Name: ${run.crafts[i].name}`
+                para2.textContent = `Location ${run.crafts[i].location}`;
+                para3.textContent = `Description: ${run.crafts[i].description}`;
                 sec.appendChild(para1);
                 sec.appendChild(para2);
                 sec.appendChild(para3);
                 document.querySelector("#myStuff").appendChild(sec)
-                break;
+                
             } else if (dataSet === "furniture") {
 
-                let para1 = document.createElement('p');
-                let para2 = document.createElement('p');
-                let para3 = document.createElement('p');
-                para1.textContent = `Name: ${array[1].name}`
-                para2.textContent = `Location ${array[1].location}`;
-                para3.textContent = `Description: ${array[1].description}`;
-                sec.appendChild(para1);
-                sec.appendChild(para2);
-                sec.appendChild(para3);
-                document.querySelector("#myStuff").appendChild(sec)
-                break;
-            }
-            else if (dataSet === "electronics") {
-
-                let para1 = document.createElement('p');
-                let para2 = document.createElement('p');
-                let para3 = document.createElement('p');
                 para1.textContent = `Name: ${array[i].name}`
                 para2.textContent = `Location ${array[i].location}`;
                 para3.textContent = `Description: ${array[i].description}`;
@@ -59,7 +43,17 @@ let makeData = (dataSet) => {
                 sec.appendChild(para2);
                 sec.appendChild(para3);
                 document.querySelector("#myStuff").appendChild(sec)
-                break;
+                
+            }
+            else if (dataSet === "electronics") {
+
+                para1.textContent = `Name: ${run.electronics[i].name}`
+                para2.textContent = `Location ${run.electronics[i].location}`;
+                para3.textContent = `Description: ${run.electronics[i].description}`;
+                sec.appendChild(para1);
+                sec.appendChild(para2);
+                sec.appendChild(para3);
+                document.querySelector("#myStuff").appendChild(sec)
             } else {
 
                 let para1 = document.createElement('p');
