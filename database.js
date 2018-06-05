4952f1aef74ac437dcb95b54ab56694508dbde24
William Kimball <wkkimball043@gmail.com>
2018-06-05 09:59:34 -0500

    fixed problem

--- database.js
+++ database.js
@@ -14,9 +14,6 @@ let run = loadDatabase("HomeInventoryDatabase");
 let makeData = (dataSet) => {
 
 
-    // console.log(array)
-
-
     if (dataSet === "crafts") {
         let array = run.crafts;
         let sec = document.createElement("section");
@@ -76,26 +73,26 @@ let makeData = (dataSet) => {
     } else {
         for (let obj in run) {
         let array = run[obj];
+        let sec = document.createElement("section");
         for (i = 0; i < array.length; i++) {
             
-            let sec = document.createElement("section");
-            console.log(array)
-            array.forEach(element => {
-                console.log(element)
-                let para1 = document.createElement('p');
-                let para2 = document.createElement('p');
-                let para3 = document.createElement('p');
-                para1.textContent = `Name: ${element.name}`
-                para2.textContent = `Location ${element.location}`;
-                para3.textContent = `Description: ${element.description}`;
-                sec.appendChild(para1);
-                sec.appendChild(para2);
-                sec.appendChild(para3);
-                document.querySelector("#myStuff").appendChild(sec)
+            let para1 = document.createElement('p');
+            let para2 = document.createElement('p');
+            let para3 = document.createElement('p');
+
+            para1.textContent = `Name: ${array[i].name}`
+            para2.textContent = `Location ${array[i].location}`;
+            para3.textContent = `Description: ${array[i].description}`;
+
+            sec.appendChild(para1);
+            sec.appendChild(para2);
+            sec.appendChild(para3);
+
+            document.querySelector("#myStuff").appendChild(sec)
             }
-            )}
 
     }
+}
 }
 
 
